cc7e96bebdc01f6ecc3e29c1fddc32efdff54fd4
ewaugh2 <ewaugh2@uc.cl>
2019-05-30 18:44:57 -0700

    Basic custom imput from main menu TEST

---
+++ ECS189L/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager IM;
+
+    [SerializeField] public Dictionary<string, KeyCode> buttonKeys;
+
+    void Awake()
+    {
+        if (IM == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            IM = this;
+        }
+        else if (IM != this)
+        {
+            Destroy(gameObject);
+        }
+
+        buttonKeys = new Dictionary<string, KeyCode>();
+
+        buttonKeys["Up1"] = KeyCode.UpArrow;
+        buttonKeys["Up2"] = KeyCode.W;
+        buttonKeys["Up3"] = KeyCode.I;
+        buttonKeys["Up4"] = KeyCode.T;
+
+
+        buttonKeys["Down1"] = KeyCode.DownArrow;
+        buttonKeys["Down2"] = KeyCode.S;
+        buttonKeys["Down3"] = KeyCode.K;
+        buttonKeys["Down4"] = KeyCode.G;
+
+        buttonKeys["Left1"] = KeyCode.LeftArrow;
+        buttonKeys["Left2"] = KeyCode.A;
+        buttonKeys["Left3"] = KeyCode.J;
+        buttonKeys["Left4"] = KeyCode.F;
+
+        buttonKeys["Right1"] = KeyCode.RightArrow;
+        buttonKeys["Right2"] = KeyCode.D;
+        buttonKeys["Right3"] = KeyCode.L;
+        buttonKeys["Right4"] = KeyCode.H;
+
+        buttonKeys["Attack1"] = KeyCode.Space;
+        buttonKeys["Attack2"] = KeyCode.Q;
+        buttonKeys["Attack3"] = KeyCode.U;
+        buttonKeys["Attack4"] = KeyCode.R;
+    }
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GetButtonDown( string buttonName )
+    {
+        if( buttonKeys.ContainsKey(buttonName) == false )
+        {
+            Debug.LogError("InputManager::GetButtonDown >> No button named " + buttonName);
+            return false;
+        }
+
+        return Input.GetKeyDown( buttonKeys[buttonName] );
+    }
+}
---
+++ ECS189L/Assets/Scripts/Input/SetKeyMainMenu.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SetKeyMainMenu : MonoBehaviour
+{
+    Transform controls;
+    Transform player;
+    Transform buttonInit;
+
+    GameObject button;
+    GameObject child;
+    string buttonText;
+
+    Event keyEvent;
+    KeyCode newKey;
+    string key;
+
+    bool waitingForKey;
+
+    List<string> buttonList;
+
+    public void Start()
+    {
+        waitingForKey = false;
+
+        buttonList = new List<string>();
+        buttonList.Add("UpButton");
+        buttonList.Add("DownButton");
+        buttonList.Add("LeftButton");
+        buttonList.Add("RightButton");
+        buttonList.Add("AttackButton");
+
+        controls = this.transform;
+
+        for (int i = 0; i < 4; i++)
+        {
+            player = controls.GetChild(i);
+
+            for (int j = 0; j < player.childCount; j++)
+            {
+                buttonInit = player.GetChild(j);
+
+                
+                if (buttonInit.name == "AttackButton")
+                {
+                    key = "Attack"+(i+1).ToString();
+                    buttonInit.GetComponentInChildren<Text>().text = InputManager.IM.buttonKeys[key].ToString();
+                }
+                else if (buttonInit.name == "UpButton")
+                {
+                    key = "Up"+(i+1).ToString();
+                    buttonInit.GetComponentInChildren<Text>().text = InputManager.IM.buttonKeys[key].ToString();
+                }
+                else if (buttonInit.name == "DownButton")
+                {
+                    key = "Down"+(i+1).ToString();
+                    buttonInit.GetComponentInChildren<Text>().text = InputManager.IM.buttonKeys[key].ToString();
+                }
+                else if (buttonInit.name == "LeftButton")
+                {
+                    key = "Left"+(i+1).ToString();
+                    buttonInit.GetComponentInChildren<Text>().text = InputManager.IM.buttonKeys[key].ToString();
+                }
+                else if (buttonInit.name == "RightButton")
+                {
+                    key = "Right"+(i+1).ToString();
+                    buttonInit.GetComponentInChildren<Text>().text = InputManager.IM.buttonKeys[key].ToString();
+                }
+            }
+        }
+        
+
+        // if (keyEvent.isKey && waitingForKey)
+        // {
+        //     newKey = keyEvent.KeyCode;
+        //     waitingForKey = false;
+        // }
+    }
+
+    public void Update()
+    {
+        button = EventSystem.current.currentSelectedGameObject;
+
+        if (button.name != "Settings")
+        {
+            waitingForKey = false;
+            OnGUI();
+        }
+    }
+
+    public void OnGUI()
+    {
+        if (button != null)
+        {
+            if (buttonList.Contains(button.name) && !waitingForKey)
+            {
+                waitingForKey = true;
+                // Debug.Log(button.name);
+            }
+            else
+            {
+                waitingForKey = false;
+            }
+        }
+
+        if (waitingForKey)
+        {
+            keyEvent = Event.current;
+            
+            if (keyEvent != null)
+            {
+                if (keyEvent.isKey)
+                {
+                    key = button.name.Substring(0, button.name.Length - 6) + button.transform.parent.name.Substring(6, 1);
+                    newKey = keyEvent.keyCode;
+
+                    InputManager.IM.buttonKeys[key] = newKey;
+                    button.GetComponentInChildren<Text>().text = InputManager.IM.buttonKeys[key].ToString();
+                    // waitingForKey = false;
+
+                    // Debug.Log(key);
+                    // Debug.Log(newKey);
+                    // Debug.Log(" ");
+                }
+            }
+        }
+        
+            
+    }
+
+    public void StartAssignment(string keyName)
+    {
+        if (!waitingForKey)
+        {
+            StartCoroutine(AssignKey(keyName));
+        }
+    }
+
+    public void SendText(string text)
+    {
+        buttonText = text;
+    }
+
+    IEnumerator WaitForKey()
+    {
+        while(!keyEvent.isKey)
+        {
+            yield return null;
+        }
+    }
+
+    public IEnumerator AssignKey(string keyName)
+    {
+        waitingForKey = true;
+
+        yield return WaitForKey();
+
+        key = button.GetComponentInChildren<Text>().text;
+        var number = button.transform.parent.name;
+        Debug.Log(key + number.GetType() + number);
+
+        // switch (keyName)
+        // {
+        //     case "UpButton":
+        //         InputManager.IM.buttonKeys[button.GetComponentInChildren<Text>().text];
+        //     default:
+        // }
+
+        yield return null;
+    }
+}
