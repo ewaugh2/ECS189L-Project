62499d814d9677dc89278fb8f108e5683e3a515a
FernandoPieressa <fapieressa@uc.cl>
2019-06-02 16:52:16 -0700

    Healthbar

---
+++ ECS189L/Assets/Scripts/GameUI/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    private Transform bar;
+
+    private void Start()
+    {
+        Transform bar = transform.Find("BarSprite");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+      bar.localScale = new Vector3(sizeNormalized, 1f);
+      float move = (1-sizeNormalized) * 100;
+      bar.localPosition = new Vector3(bar.localPosition.x - move, bar.localPosition.y);
+    }
+}
