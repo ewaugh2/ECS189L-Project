e8a7a46838755f965ecfe10490018774e6b33b20
FernandoPieressa <fapieressa@uc.cl>
2019-06-02 17:29:48 -0700

    Healthbar color

--- ECS189L/Assets/Scripts/GameUI/HealthBar.cs
+++ ECS189L/Assets/Scripts/GameUI/HealthBar.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
 
     private Transform bar;
+    private GameObject health;
 
     private void Start()
     {
-        Transform bar = transform.Find("BarSprite");
+        bar = transform.Find("BarSprite");
+        health = GameObject.Find("BarSprite");
     }
 
     public void SetSize(float sizeNormalized)
@@ -17,5 +20,6 @@ public class HealthBar : MonoBehaviour
       bar.localScale = new Vector3(sizeNormalized, 1f);
       float move = (1-sizeNormalized) * 100;
       bar.localPosition = new Vector3(bar.localPosition.x - move, bar.localPosition.y);
+      health.GetComponent<Image>().color = new Color32((-200*sizeNormalized)+255,(((-200*sizeNormalized)+255)*-1)+255,0,255);
     }
 }
