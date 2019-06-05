ae62cb9ebce2d5ac36c0bfd4026c4c8375f84957
FernandoPieressa <fapieressa@uc.cl>
2019-06-04 19:41:42 -0700

    Split Screen

--- ECS189L/Assets/Scripts/PlayerSpawner.cs
+++ ECS189L/Assets/Scripts/PlayerSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
     private GameObject playerPrefab;
+    private GameObject playerUiPrefab;
 
     public PlayerSpawner()
     {
@@ -24,16 +25,20 @@ public class PlayerSpawner : MonoBehaviour
     }
 
     //Set the prefab
-    public void setPrefab(GameObject prefab)
+    public void setPrefab(GameObject prefab, GameObject gameUi)
     {
         playerPrefab = prefab;
+        playerUiPrefab = gameUi;
     }
 
     //Spawn the player
     public void spawnPlayer()
     {
         var map = GameObject.Find("Map");
+        var canvas = GameObject.Find("Canvas");
         var player = (GameObject)Instantiate(playerPrefab, map.transform.localPosition + new Vector3(20,20,0), Quaternion.identity);
+        var playerUi = (GameObject)Instantiate(playerUiPrefab, canvas.transform.localPosition - new Vector3(580,-20,0), Quaternion.identity);
         player.transform.parent = GameObject.Find("Map").transform;
+        playerUi.transform.parent = GameObject.Find("Canvas").transform;
     }
 }
